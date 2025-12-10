85baa456d5206efc259517a6bd5e0811f4300a5e
Leandro Martinez <lmartine@unicamp.br>
2025-12-10 09:51:19 -0300

    atom types

--- src/LAPM.jl
+++ src/LAPM.jl
@@ -10,7 +10,9 @@ using PDBTools:
     MValueModel,
     mvalue_delta_sasa,
     parse_mvalue_server_sasa,
-    read_pdb
+    read_pdb,
+    StringType
+using EasyFit
 
 export MoeserHorinek, AutonBolen
 export plot_mvalue
@@ -298,7 +300,11 @@ function plot_experimental(
     plot!(plt, xlabel="Experimental")
     plot!(plt, ylabel="LAPM prediction ($(ylab(model)))")
     plot!(plt, size=(400,400))
+    fit = fitlinear(tot_exp, tot_pred)
+    plot!(legend_title="a=$(round(fit.a; digits=3))\nb=$(round(fit.b; digits=3))\nR2=$(round(fit.R2; digits=3))", legend=:topleft)
+    plot!(xlims=(-3.5,0.2), ylims=(-3.5,0.2))
     return plt
+#    return fit
 end
 
 end
--- src/data/sasa_auton_bolen_server/creamer.jl
+++ src/data/sasa_auton_bolen_server/creamer.jl
@@ -1,3 +1,36 @@
+function bondi_atom_type(at::Atom)
+    types = Dict{StringType,StringType}(
+        "N" => "N",
+        "CA" => "CA",
+        "C" => "C",
+        "O" => "O",
+    )
+    nat = name(at)
+    type = if haskey(types, nat)
+        types[name(at)]
+    elseif element(at) == "H"
+        "H"
+    elseif element(at) == "Fe"
+        "Fe"
+    else
+        "side-chain"
+    end
+    return type
+end
+
+function bondi_atom_radii(at_type::AbstractString) 
+    radii = Dict{StringType,Float32}(
+        "N" => 1.55,
+        "CA" => 1.70,
+        "C" => 1.80,
+        "O" => 1.52,
+        "Fe" => 0.64,
+        "side-chain" => 1.80,
+        "H" => 0.0,
+    )
+    return radii[at_type]
+end
+
 const creamer_atom_radii = Dict{String, Float32}(
     "Nsp2" => 1.64,
     "Nsp3" => 1.64,
@@ -282,3 +315,111 @@ function creamer_sasa_restype(atoms::AbstractVector{<:PDBTools.Atom})
     end
     return sasas
 end
+
+# voltar
+
+bondi_sasa(_, atoms) = bondi_sasa_restype(atoms)
+function bondi_sasa_restype(atoms::AbstractVector{<:PDBTools.Atom})
+    sasas = Dict{String,Dict}()
+    sasa_atoms = sasa_particles(atoms;
+        atom_type = bondi_atom_type,
+        atom_radius_from_type = bondi_atom_radii,
+    )
+    sel_bb = _Selector(isbackbone, Ref(first(eachresidue(atoms))))
+    sel_sc = _Selector(issidechain, Ref(first(eachresidue(atoms))))
+    for res in eachresidue(atoms)
+        rname = resname(res)
+        if !haskey(sasas, rname)
+            sasas[rname] = Dict(:sc => (0.0, 0.0, 0.0), :bb => (0.0, 0.0, 0.0))
+        end
+    end
+    for res in eachresidue(atoms)
+        sel_bb.residue[] = res
+        sel_sc.residue[] = res
+        sasa_res_bb = sasa(sasa_atoms, sel_bb)
+        sasa_res_sc = sasa(sasa_atoms, sel_sc)
+        rname = resname(res)
+        cr = creamer_sasas[rname]
+        csc = sasas[rname][:sc]
+        cbb = sasas[rname][:bb]
+        sasas[rname] = Dict(
+            :sc => (
+                csc[1] + cr.sc_lower - sasa_res_sc,
+                csc[2] + 0.5 * (cr.sc_lower + cr.sc_upper) - sasa_res_sc,
+                csc[3] + cr.sc_upper - sasa_res_sc,
+            ),
+            :bb => (
+                cbb[1] + cr.bb_lower - sasa_res_bb,
+                cbb[2] + 0.5 * (cr.bb_lower + cr.bb_upper) - sasa_res_bb,
+                cbb[3] + cr.bb_upper - sasa_res_bb,
+            )
+        )
+    end
+    return sasas
+end
+
+creamer_sasa_new(_, atoms) = creamer_sasa_restype_new(atoms)
+function creamer_sasa_restype_new(atoms::AbstractVector{<:PDBTools.Atom})
+    sasas = Dict{String,Dict}()
+    sasa_atoms = sasa_particles(atoms;
+        atom_type = creamer_atom_type,
+        atom_radius_from_type = type -> creamer_atom_radii[type],
+    )
+    sel_bb = _Selector(isbackbone, Ref(first(eachresidue(atoms))))
+    sel_sc = _Selector(issidechain, Ref(first(eachresidue(atoms))))
+    for res in eachresidue(atoms)
+        rname = resname(res)
+        if !haskey(sasas, rname)
+            sasas[rname] = Dict(:sc => (0.0, 0.0, 0.0), :bb => (0.0, 0.0, 0.0))
+        end
+    end
+    for res in eachresidue(atoms)
+        sel_bb.residue[] = res
+        sel_sc.residue[] = res
+        sasa_res_bb = sasa(sasa_atoms, sel_bb)
+        sasa_res_sc = sasa(sasa_atoms, sel_sc)
+        rname = resname(res)
+        cr = creamer_sasas_new[rname]
+        csc = sasas[rname][:sc]
+        cbb = sasas[rname][:bb]
+        sasas[rname] = Dict(
+            :sc => (
+                csc[1] + cr.sc_lower - sasa_res_sc,
+                csc[2] + 0.5 * (cr.sc_lower + cr.sc_upper) - sasa_res_sc,
+                csc[3] + cr.sc_upper - sasa_res_sc,
+            ),
+            :bb => (
+                cbb[1] + cr.bb_lower - sasa_res_bb,
+                cbb[2] + 0.5 * (cr.bb_lower + cr.bb_upper) - sasa_res_bb,
+                cbb[3] + cr.bb_upper - sasa_res_bb,
+            )
+        )
+    end
+    return sasas
+end
+
+const creamer_sasas_new = Dict{
+    String,
+    @NamedTuple{bb_lower::Float64, bb_upper::Float64, sc_lower::Float64, sc_upper::Float64}
+}(
+  "ALA" => (bb_lower = 20.7273, bb_upper = 38.2849, sc_lower = 43.4031, sc_upper = 58.0849),
+  "ARG" => (bb_lower = 18.3347, bb_upper = 33.3763, sc_lower = 148.83, sc_upper = 168.118),
+  "ASN" => (bb_lower = 18.9146, bb_upper = 33.7982, sc_lower = 83.1922, sc_upper = 95.0593),
+  "ASP" => (bb_lower = 19.4371, bb_upper = 34.6858, sc_lower = 78.5996, sc_upper = 89.4861),
+  "CYS" => (bb_lower = 20.9307, bb_upper = 37.608, sc_lower = 54.5206, sc_upper = 70.6493),
+  "GLN" => (bb_lower = 17.7608, bb_upper = 33.6317, sc_lower = 102.378, sc_upper = 118.62),
+  "GLU" => (bb_lower = 19.0967, bb_upper = 33.4637, sc_lower = 100.832, sc_upper = 113.39),
+  "GLY" => (bb_lower = 54.2184, bb_upper = 74.7222, sc_lower = 0.0, sc_upper = 0.0),
+  "HIS" => (bb_lower = 15.4983, bb_upper = 32.6304, sc_lower = 103.378, sc_upper = 118.486),
+  "ILE" => (bb_lower = 16.6421, bb_upper = 31.0321, sc_lower = 97.4824, sc_upper = 117.418),
+  "LEU" => (bb_lower = 15.8228, bb_upper = 31.9691, sc_lower = 96.9378, sc_upper = 117.052),
+  "LYS" => (bb_lower = 19.636, bb_upper = 34.4396, sc_lower = 130.759, sc_upper = 138.781),
+  "MET" => (bb_lower = 17.9485, bb_upper = 32.8476, sc_lower = 99.2576, sc_upper = 125.076),
+  "PHE" => (bb_lower = 16.2814, bb_upper = 31.5632, sc_lower = 116.243, sc_upper = 137.095),
+  "PRO" => (bb_lower = 19.9066, bb_upper = 27.257, sc_lower = 80.2617, sc_upper = 95.5204),
+  "SER" => (bb_lower = 24.9099, bb_upper = 37.2971, sc_lower = 56.0964, sc_upper = 66.5874),
+  "THR" => (bb_lower = 19.7255, bb_upper = 32.0237, sc_lower = 72.6126, sc_upper = 85.7771),
+  "TRP" => (bb_lower = 16.8528, bb_upper = 29.9011, sc_lower = 149.543, sc_upper = 167.217),
+  "TYR" => (bb_lower = 18.8791, bb_upper = 31.7213, sc_lower = 127.086, sc_upper = 146.505),
+  "VAL" => (bb_lower = 16.8794, bb_upper = 32.0856, sc_lower = 78.1454, sc_upper = 94.0587),
+)
